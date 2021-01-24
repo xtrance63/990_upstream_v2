c89bf549132eb48098d4050c9a71a5cca5702241
switchgott <switchgott@gmx.de>
2021-01-24 22:19:49 +0100

    implement DISABLE_TRACE_PRINTK

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -709,6 +709,9 @@ do {									\
  * let gcc optimize the rest.
  */
 
+#ifdef CONFIG_DISABLE_TRACE_PRINTK
+#define trace_printk pr_debug
+#else
 #define trace_printk(fmt, ...)				\
 do {							\
 	char _______STR[] = __stringify((__VA_ARGS__));	\
@@ -717,6 +720,7 @@ do {							\
 	else						\
 		trace_puts(fmt);			\
 } while (0)
+#endif
 
 #define do_trace_printk(fmt, args...)					\
 do {									\
