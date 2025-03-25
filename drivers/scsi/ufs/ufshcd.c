2089b2eb8588a5d878353e14bb35bbf6ea084ed9
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-25 21:48:43 +0200

    drivers: ufs: fix warning

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6437,12 +6437,11 @@ static void __ufshcd_transfer_req_compl(struct ufs_hba *hba, int reason,
 	int result;
 	int index;
 	int i;
-	int cpu = raw_smp_processor_id();
-	unsigned int dump_index;
 	u32 offset;
 	void *dma_phy_addr;
-
 #if defined(CONFIG_UFS_DATA_LOG)
+	int cpu = raw_smp_processor_id();
+	unsigned int dump_index;
 #if defined(CONFIG_UFS_DATA_LOG_MAGIC_CODE)
 	struct scatterlist *sg;
 	int sg_segments;
